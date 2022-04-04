b2b84e5bbc728446f86fcd86e9fb397d480ff5ea
Miles Okamoto <milesokamoto@utexas.edu>
2022-04-04 10:30:28 -0500

    Update polls_hm.py

--- exercises_5/polls_hm.py
+++ exercises_5/polls_hm.py
@@ -5,48 +5,96 @@ from scipy.stats import multivariate_normal, truncnorm, gamma
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-n_iter = 1000
-burn = 500
+ENCODE_EDU = {
+    "NoHS": 0,
+    "HS": 1,
+    "SomeColl": 2,
+    "Bacc": 3
+}
 
-data = pd.read_csv("experimental/mokamoto/polls.csv")
-data_cleaned = data[data["bush"].notna()]
-states = data_cleaned[["state"]]
-groups = data_cleaned.groupby('state')
-states = list(groups.groups.keys())
-X = []
-y = []
-for state in states:
-    df_group = groups.get_group(state)
-    X.append(df_group[["female", "black", "weight"]].to_numpy())
-    y.append(df_group['bush'].to_numpy())
-# X = data[["state", "female", "black", "weight", "edu", "age"]] # need to categoricalize edu, age
-# X = data_cleaned[["female", "black"]]
-# y = data_cleaned[["bush"]]
+ENCODE_AGE = {
+    "18to29": 0,
+    "30to44": 1,
+    "45to64": 2,
+    "65plus": 3
+}
 
-n_betas = X[0].shape[1]
-groups_idx = pd.get_dummies(states[["state"]])
-n_groups = groups_idx.shape[1]
+class ProbitRegression:
+    def __init__(self, X, y, groups, n_iter:int = 1000, burn:int=500):
+        self.n_iter = n_iter
+        self.burn = burn
 
-B_0 = 1
-betas = np.ones((n_betas, n_groups))
-b_0 = np.ones(n_betas)
-z = [np.ones(len(x)) for x in y]
+        self.n_betas = X.shape[1]
+        self.groups_idx = pd.get_dummies(groups)
+        self.n_groups = self.groups_idx.shape[1]
+        self.X_sparse = self.get_X_sparse(X)
+        self.y = y
 
-# update_betas
-for group in range(n_groups):
-    B = np.linalg.inv(1/B_0*np.eye(n_betas) + X[group].T @ X[group])
-    b = B @ (b_0 / B_0 + X[group].T @ z[group])
-    betas[:, group] = multivariate_normal.rvs(mean=b, cov=B)
+        self.B_0 = 1
+        self.betas = np.ones((self.n_betas * self.n_groups))
+        self.b_0 = np.ones(self.n_betas * self.n_groups)
+        self.z = np.ones(len(y)).T
 
-# update B_0
-alpha = (n_groups + 1) / 2
-beta = 0.5 * (((betas - b_0[:, None]) ** 2).sum() + 1)
-B_0 = 1 / gamma.rvs(a=alpha, scale=1 / beta)
+        self.traces = {'betas': np.zeros((self.n_iter, self.n_betas, self.n_groups)),
+            'tau_squared': np.zeros(self.n_iter),
+            'm': np.zeros((self.n_iter, self.n_betas))}
 
-# update b_0
-cov = B_0 / n_groups * np.eye(n_betas)
-mean = cov / B_0 @ betas.sum(axis=1)
-b_0 = multivariate_normal.rvs(mean=mean, cov=cov)
+    def get_X_sparse(self, X):
+        return np.array([
+        np.concatenate(
+            [
+                np.zeros(self.n_betas*np.argmax(self.groups_idx.iloc[row])), X[row], np.zeros(self.n_betas* (self.groups_idx.shape[1]-1-np.argmax(self.groups_idx.iloc[row])))
+            ]
+        ) for row in range(len(X))])
 
-# update z
-z[group] = truncnorm.rvs([-np.inf if x == 0 else 0 for x in y[group]], [0 if x == 0 else np.inf for x in y[group]], loc=0, scale=1)
+    def update_betas(self):
+        B = np.linalg.inv(1/self.b_0*np.eye(self.n_betas*self.n_groups) + self.X_sparse.T @ self.X_sparse)
+        b = B @ (self.b_0 / self.b_0 + self.X_sparse.T @ self.z)
+        self.betas = multivariate_normal.rvs(b, B)
+
+    # update B_0
+    def update_B_0(self):
+        alpha = (self.n_groups + 1) / 2
+        beta = 0.5 * (((self.betas - self.b_0) ** 2).sum() + 1)
+        self.b_0 = 1 / gamma.rvs(a=alpha, scale=1 / beta)
+
+    # update b_0
+    def update_b_0(self):
+        cov = self.b_0 / self.n_groups * np.eye(self.n_betas*self.n_groups)
+        mean = cov / self.b_0 @ self.betas
+        self.b_0 = multivariate_normal.rvs(mean=mean, cov=cov)
+
+    # update z
+    def update_z(self):
+        self.z = truncnorm.rvs([-np.inf if x == 0 else 0 for x in self.y], [0 if x == 0 else np.inf for x in self.y], loc=0, scale=1)
+
+    def update_traces(self, it):
+        self.traces['betas'][it, :, :] = self.betas.reshape(self.n_betas, len(self.y))
+        self.traces['b_0'][it, :] = self.b_0
+        self.traces['B_0'][it] = self.B_0
+
+    def fit(self):
+        for it in tqdm(range(self.n_iter + self.burn)):
+            self.update_B_0()
+            self.update_betas()
+            self.update_b_0()
+            self.update_z()
+            if it >= self.burn:
+                self.update_traces(it-self.burn)
+
+
+def main():
+    """main method"""
+    data = pd.read_csv("experimental/mokamoto/polls.csv")
+    data_cleaned = data[data["bush"].notna()].sort_values("state")
+    data_cleaned["edu_code"] = [ENCODE_EDU[x] for x in data_cleaned["edu"]]
+    data_cleaned["age_code"] = [ENCODE_AGE[x] for x in data_cleaned["age"]]
+    data_cols = ["female", "black", "weight", "edu_code", "age_code"]
+    groups = data_cleaned[["state"]]
+    X = data_cleaned[data_cols].to_numpy()
+    y = data_cleaned[["bush"]].to_numpy()
+    pr = ProbitRegression(X, y, groups)
+    pr.fit()
+
+if __name__ == "__main__":
+    main()
